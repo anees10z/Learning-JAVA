f4a70e74471148f69463b7493e24641d381f27c6
anees10z <khanmdanis145@gmail.com>
2026-08-14 12:13:52 +0530

    Merge sort done

---
+++ LinkedList/JCF_LL/MergeSortOnLL.java
@@ -0,0 +1,133 @@
+package LinkedList.JCF_LL;
+
+public class MergeSortOnLL {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    private Node head;
+    private int size;
+
+    // Add node at first
+    public void addInFirst(int data) {
+        Node newNode = new Node(data);
+        size++;
+
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        newNode.next = head;
+        head = newNode;
+    }
+
+    // Add node at last
+    public void addInLast(int data) {
+        Node newNode = new Node(data);
+        size++;
+
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+    }
+
+    public void displayLL() {
+        Node temp = head;
+
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+
+        System.out.println("null");
+    }
+
+    // Find middle
+    private Node getMid(Node head) {
+        Node slow = head;
+        Node fast = head.next;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    // Merge two sorted linked lists
+    private Node merge(Node head1, Node head2) {
+        Node merged = new Node(-1);
+        Node temp = merged;
+
+        while (head1 != null && head2 != null) {
+            if (head1.data <= head2.data) {
+                temp.next = head1;
+                head1 = head1.next;
+            } else {
+                temp.next = head2;
+                head2 = head2.next;
+            }
+            temp = temp.next;
+        }
+
+        if (head1 != null) {
+            temp.next = head1;
+        }
+
+        if (head2 != null) {
+            temp.next = head2;
+        }
+
+        return merged.next;
+    }
+
+    // Recursive merge sort
+    private Node mergeSort(Node head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        Node mid = getMid(head);
+        Node rightHalf = mid.next;
+        mid.next = null;
+
+        Node newLeft = mergeSort(head);
+        Node newRight = mergeSort(rightHalf);
+
+        return merge(newLeft, newRight);
+    }
+
+    // Public method to sort the linked list
+    public void sort() {
+        head = mergeSort(head);
+    }
+
+    public static void main(String[] args) {
+        MergeSortOnLL list = new MergeSortOnLL();
+
+        list.addInFirst(4);
+        list.addInLast(3);
+        list.addInLast(2);
+        list.addInLast(1);
+
+        list.displayLL();
+
+        list.sort();
+
+        list.displayLL();
+    }
+}
