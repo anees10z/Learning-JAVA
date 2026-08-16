f1ae2abe8ee27a51a1a3f255d27c318198e2db28
anees10z <khanmdanis145@gmail.com>
2026-08-16 13:45:38 +0530

    basic done

--- Hashing/HashMaps/HashMapBasics.java
+++ Hashing/HashMaps/HashMapBasics.java
@@ -1,13 +1,53 @@
 package Hashing.HashMaps;
 
 import java.util.HashMap;
+import java.util.Set;
 
 public class HashMapBasics {
     public static void main(String[] args) {
+        // create
         HashMap<String, Integer> hm = new HashMap<>();
         System.out.println(hm);
+
+        // insert
         hm.put("mango", 60);
         hm.put("banana", 70);
+        hm.put("orange", 40);
+        hm.put("watermelon", 25);
+        hm.put("grapes", 35);
         System.out.println(hm);
+
+        // get
+        int cost = hm.get("banana");
+        System.out.println(cost);
+        System.out.println(hm.get("pear"));
+
+        // ContainsKey
+        System.out.println(hm.containsKey("mango"));
+        System.out.println(hm.containsKey("apple"));
+
+        // remove
+        System.out.println(hm.remove("mango"));
+        System.out.println(hm.remove("apple"));
+
+        // size
+        System.out.println(hm.size());
+
+        // isEmpty
+        System.out.println(hm.isEmpty());
+
+        // ! clear
+        // hm.clear();
+        // System.out.println(hm);
+
+        // entrySet
+        System.out.println(hm.entrySet());
+
+        // iteration on hashmap using set
+        Set<String> keys = hm.keySet();
+        for (String k : keys) {
+            System.out.println("Key: " + k + " , value: " + hm.get(k));
+        }
+        ;
     }
 }
